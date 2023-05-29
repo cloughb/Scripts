9271620cfb97aabef6d84af598ba7ca51c978a40
Benjamin J. Clough <bclough@bsbc.local>
2023-05-28 21:49:48 -0500

    Script to prank someone.

---
+++ ohya.ps1
@@ -0,0 +1,18 @@
+
+Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$speak.Speak('oooooo ya suck it <Insert the person who will be sucking it>')
+
+
+
+
+
+#Obviously, insert the location of whatever felacio looking gif you would like
+Set-Location 'C:\Users\bclough.BSBC\Documents\Scripts Misc\Get Good Hack'
+.\suck-it-mime.gif -WindowStyle maximized
+For ($i = 0; $i -lt 100; $i++) {
+    Add-Type -AssemblyName System.speech
+    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $speak.Speak('oooooo ya                                             suck it                   <Insert the person who will be sucking it>')
+   
+}
